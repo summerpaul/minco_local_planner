a28fe40087b743d0c88c0a0f14ae21b1fc05d443
yjyxiayk <yjyxiayk@sunnyoptical.com>
2023-09-05 00:23:58 +0800

    优化代码结构

--- minco_local_planner/core/trajectory_optimizer/trajectory_optimizer.cpp
+++ minco_local_planner/core/trajectory_optimizer/trajectory_optimizer.cpp
@@ -2,10 +2,10 @@
  * @Author: Xia Yunkai
  * @Date:   2023-08-29 20:48:57
  * @Last Modified by:   Xia Yunkai
- * @Last Modified time: 2023-08-29 20:51:31
+ * @Last Modified time: 2023-09-05 00:14:04
  */
-#include <iostream>
-
 #include "trajectory_optimizer.h"
+
+#include <iostream>
 
 namespace minco_local_planner::trajectory_optimizer {}
